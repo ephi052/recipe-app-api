ca864a9ac02bd0e1fcf59ed04e82547186b325d6
unknown <cbf252@gmail.com>
2022-05-25 16:54:20 +0300

    TDD test

---
+++ app/app/calc.py
@@ -0,0 +1,10 @@
+"""
+Calculator functions
+"""
+def add(x, y):
+    """ add x to y and return result """
+    return x + y
+
+def sub(x, y):
+    """ subtract x form y and return the result """
+    return 0
---
+++ app/app/test.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """test the calc modul"""
+    def test_add_numbers(self):
+        """test adding numbers together"""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res,11)
+
+    def test_subtract_numbers(self):
+        """test subtracting numbers"""
+        res = calc.sub(2, 4)
+
+        self.assertEqual(res, 2)
