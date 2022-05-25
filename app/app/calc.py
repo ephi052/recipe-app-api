cc86bca07a46adf2d01046224744882e927f6b84
unknown <cbf252@gmail.com>
2022-05-25 17:04:12 +0300

    TDD lint fix

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,10 +1,14 @@
 """
 Calculator functions
 """
+
+
 def add(x, y):
     """ add x to y and return result """
     return x + y
 
+
 def sub(x, y):
     """ subtract x form y and return the result """
     return x - y
+    
--- app/app/test.py
+++ app/app/test.py
@@ -5,13 +5,14 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """test the calc modul"""
     def test_add_numbers(self):
         """test adding numbers together"""
         res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """test subtracting numbers"""
