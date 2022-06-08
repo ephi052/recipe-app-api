f00c29f5c0d4024fee8524d0244d6f361f88cff4
unknown <cbf252@gmail.com>
2022-06-08 14:02:46 +0300

    e39 fix anf linting

--- app/app/calc.py
+++ app/app/calc.py
@@ -8,6 +8,6 @@ def add(x, y):
     return x + y
 
 
-def sub(x, y):
+def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,12 +1,11 @@
 """
-Django command to wait for the data base to be avilable
+Django command to wait for the database to be available.
 """
-
-from django.core.management import BaseCommand
+from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to wait for data base"""
+    """Django command to wait for database."""
 
-    def handle(self,*args, **options):
+    def handle(self, *args, **options):
         pass
