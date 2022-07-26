8ea5c5cbc39f4ba6aefc2f6fff68f10aca13ba6e
unknown <cbf252@gmail.com>
2022-07-26 10:19:11 +0300

    interbnal error not found

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,6 +1,8 @@
 """
 test tags api
 """
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -8,7 +10,10 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import (
+    Tag,
+    Recipe,
+)
 from recipe.serializers import TagSerializer
 
 TAG_URL = reverse('recipe:tag-list')
@@ -94,3 +99,45 @@ class PrivateTagsApiTests(TestCase):
         tags = Tag.objects.filter(user=self.user)  # get all tags for user
         self.assertFalse(tags.exists())
         # check that the tag is deleted by checking that it doesn't exist
+
+    def test_filter_tag_assigned_to_recipes(self):
+        """Test returning tags with assigned recipes"""
+        tag1 = Tag.objects.create(user=self.user, name='Breakfast')
+        tag2 = Tag.objects.create(user=self.user, name='Lunch')
+        recipe = Recipe.objects.create(
+            title='Coriander eggs on toast',
+            time_minutes=10,
+            price=Decimal('5.00'),
+            user=self.user,
+        )
+        recipe.tags.add(tag1)
+        res = self.client.get(TAG_URL, {'assigned_only': 1})
+
+        serializer1 = TagSerializer(tag1)
+        serializer2 = TagSerializer(tag2)
+
+        self.assertIn(serializer1.data, res.data)
+        self.assertNotIn(serializer2.data, res.data)
+
+    def test_filterd_tags_assigned_unique(self):
+        """Test filtering tags by assigned returns unique items"""
+        tag = Tag.objects.create(user=self.user, name='Breakfast')
+        Tag.objects.create(user=self.user, name='Lunch')
+        recipe1 = Recipe.objects.create(
+            title='Pancakes',
+            time_minutes=5,
+            price=Decimal('3.00'),
+            user=self.user,
+        )
+        recipe1.tags.add(tag)
+        recipe2 = Recipe.objects.create(
+            title='Porridge',
+            time_minutes=3,
+            price=Decimal('2.00'),
+            user=self.user,
+        )
+        recipe2.tags.add(tag)
+
+        res = self.client.get(TAG_URL, {'assigned_only': 1})
+
+        self.assertEqual(len(res.data), 1)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -122,6 +122,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         instance.delete()
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Only include ingredients assigned to recipes',
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(
     mixins.DestroyModelMixin,
     mixins.UpdateModelMixin,
@@ -138,7 +149,14 @@ class BaseRecipeAttrViewSet(
         """
         Return the ingredients/tags for the authenticated user
         """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name').distinct()
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
